c7744d275d2b7175556838d976afbe35a903b15a
wiiboi69 <meereaareea@gmail.com>
2024-06-26 13:43:53 +0100

    rrr

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace rec_rewild_room_editor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
